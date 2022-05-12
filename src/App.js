f27e035df46cbe595aceab3e74e6dd0beb0043ba
bytrustu <bytrustu@gmail.com>
2022-05-13 06:32:48 +0900

    [ADD] Router 추가

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+import { Routes, Route } from 'react-router-dom'
 import Home from "./pages/Home";
+import Question from "./pages/Question";
+import Result from "./pages/Result";
 
 function App() {
   return (
-      <Home />
+      <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/question" element={<Question />} />
+          <Route path="/result" element={<Result />} />
+      </Routes>
   );
 }
 
